a1f8b58e893790db3b121b1a417efef1918ff440
Giovanni Lopez <giovannilopez9808@gmail.com>
2022-06-19 23:24:12 -0500

    Add functions.py

--- Scripts/Modules/data_model.py
+++ Scripts/Modules/data_model.py
@@ -1,7 +1,8 @@
-from pandas import read_csv, DataFrame, to_datetime
+from pandas import read_csv, DataFrame, to_datetime, to_timedelta
+from .functions import fill_number, yymmdd2yyyy_mm_dd
 
 
-class data_model:
+class SIMA_model:
     def __init__(self) -> None:
         pass
 
@@ -14,9 +15,47 @@ class data_model:
                              low_memory=False)
 
     def get_station_data(self, station: str, pollutant: str) -> DataFrame:
+        station = station.upper()
         self.select_data = DataFrame(self.data[(station, pollutant)])
+        self.select_data = self.select_data*1000
 
     def get_data_date(self, date: str) -> DataFrame:
         date = to_datetime(date)
         data = self.select_data[self.select_data.index.date == date.date()]
+        return data
+
+
+class SMARTS_model:
+    def __init__(self) -> None:
+        pass
+
+    def read(self, filename: str) -> DataFrame:
+        data = read_csv(filename,
+                        header=None,
+                        sep=" ")
+        data[0] = data[0].apply(self.format_time)
+        data.columns = ["Date", "Data"]
+        date = filename.split("/")[-1]
+        date = date.split(".")[0]
+        data = self.fill_year(data,
+                              date)
+        return data
+
+    def format_time(self, hour: float) -> str:
+        minutes = round(hour % 1*60)
+        minutes = fill_number(minutes, 2)
+        hour = int(hour)
+        hour = fill_number(hour, 2)
+        time = f"{hour}:{minutes}"
+        return time
+
+    def fill_year(self,
+                  data: DataFrame,
+                  date: int) -> DataFrame:
+        date = yymmdd2yyyy_mm_dd(date)
+        data["Date"] = data["Date"].apply(
+            lambda time: f"{date} {time}")
+        data.index = to_datetime(data["Date"])
+        data = data.drop(columns=["Date"])
+        data.index = data.index-to_timedelta("00:30:00")
         return data
---
+++ Scripts/Modules/functions.py
@@ -0,0 +1,28 @@
+from pandas import Timestamp
+
+
+def fill_number(number: int, zfill: int) -> str:
+    return str(number).zfill(zfill)
+
+
+def yymmdd2yyyy_mm_dd(date: int) -> str:
+    date = str(date)
+    date = [date[i:i+2]
+            for i in range(0,
+                           len(date),
+                           2)]
+    date[0] = f"20{date[0]}"
+    date = "-".join(date)
+    return date
+
+
+def yyyy_mm_dd2yymmdd(date: Timestamp) -> str:
+    year = str(date.year)
+    month = fill_number(date.month, 2)
+    day = fill_number(date.day, 2)
+    year = year[2:]
+    date = [year,
+            month,
+            day]
+    date = "".join(date)
+    return date
