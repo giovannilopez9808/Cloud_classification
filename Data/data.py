fd6ea86f7f2fa0e3f259f37b7546d6903ebbb987
Giovanni Lopez <giovannilopez9808@gmail.com>
2022-06-22 02:55:01 -0500

    Traslate data

---
+++ Data/data.py
@@ -0,0 +1,21 @@
+from pandas import read_csv
+
+parameter = "PM10"
+# parameter = "SR"
+data = read_csv("2021.csv",
+                header=[0, 1],
+                skiprows=[2],
+                index_col=0,
+                parse_dates=True,
+                low_memory=False)
+columns = data.columns
+columns = [(station, value)
+           for station, value in columns
+           if value == parameter]
+data = data[columns]
+# data = data[data.index.hour >= 8]
+# data = data[data.index.hour <= 19]
+data = data.resample("D").count()
+# data = data >= 11
+data = data >= 21
+print(data.sum()/365*100)
