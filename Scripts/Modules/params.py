f0c05f9e67537140877385f01860276e7fcb8b28
Giovanni Lopez <giovannilopez9808@gmail.com>
2022-06-22 02:55:24 -0500

    Add params file

---
+++ Scripts/Modules/params.py
@@ -0,0 +1,10 @@
+def get_params() -> dict:
+    params = {
+        "path SMARTS data": "SMARTS/Data",
+        "path graphics": "../Graphics",
+        "path data": "../Data",
+        "OMI folder": "OMI",
+        "SIMA folder": "SIMA",
+        "SMARTS DM": "Results_DM",
+    }
+    return params
