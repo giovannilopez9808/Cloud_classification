a42f54df832e3841213e73503c3fd7525cc50c33
Giovanni Lopez <giovannilopez9808@gmail.com>
2022-07-05 02:29:58 -0500

    Refractor to classification models

--- Scripts/Modules/classification_classic_models.py
+++ Scripts/Modules/classification_classic_models.py
@@ -1,9 +1,45 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeClassifier
+from .params import get_classification_params
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 from numpy import round
+
+
+class classification_model:
+    def __init__(self) -> None:
+        pass
+
+    def define_model(self,
+                     params: dict) -> None:
+        self.params = get_classification_params(params)
+        model_name = params["classification model"]
+        if model_name == "SVM":
+            self.model = SVM_model()
+            return
+        if model_name == "KNN":
+            self.model = KNN_model()
+            return
+        if model_name == "Random forest":
+            self.model = Random_forest_model()
+            return
+        if model_name == "Gaussian naive":
+            self.model = Gaussian_naive_model()
+            return
+        if model_name == "Decision tree":
+            self.model == Decision_tree_model()
+            return
+
+    def run(self,
+            data: list) -> None:
+        self.model.run(data,
+                       self.params)
+
+    def predict(self,
+                data: list) -> None:
+        results = self.model.predict(data)
+        return results
 
 
 class SVM_model:
@@ -45,7 +81,7 @@ class KNN_model:
         return results
 
 
-class random_forest_model:
+class Random_forest_model:
     def __init__(self) -> None:
         pass
 
---
+++ Scripts/classification_model.py
@@ -0,0 +1,63 @@
+"""
+python classification_model.py (classification_model)
+"""
+from Modules.classification_classic_models import classification_model
+from sklearn.metrics import classification_report
+from Modules.dataset_model import dataset_model
+from Modules.functions import get_labels
+from Modules.params import get_params
+from pandas import DataFrame, concat
+from os.path import join
+from sys import argv
+
+
+def define_index(data: DataFrame,
+                 model: str,
+                 operation: str) -> DataFrame:
+    text = "{} {} {}"
+    index = [text.format(model,
+                         operation,
+                         value)
+             for value in data.index]
+    data.index = index
+    return data
+
+
+params = get_params()
+params.update({
+    "comparison operations": ["ratio",
+                              "diff"],
+    "classification model": argv[1],
+    "clear sky models": ["RS"],
+    # "GHI"],
+})
+model = classification_model()
+model.define_model(params)
+results = DataFrame()
+for sky_model in params["clear sky models"]:
+    params["clear sky model"] = sky_model
+    for operation in params["comparison operations"]:
+        params["comparison operation"] = operation
+        print("-"*40)
+        print("Sky model: {}\tOperation: {}".format(sky_model,
+                                                    operation))
+        _, labels = get_labels(params)
+        dataset = dataset_model(params)
+        model.run(dataset.train)
+        result = model.predict(dataset.test)
+        partial_results = classification_report(dataset.test[1],
+                                                result,
+                                                target_names=labels,
+                                                output_dict=True)
+        partial_results = DataFrame(partial_results)
+        partial_results = define_index(partial_results,
+                                       sky_model,
+                                       operation)
+        last_index = partial_results.index[-1]
+        partial_results = partial_results.drop(last_index)
+        results = concat([results,
+                          partial_results])
+filename = f"{params['classification model']}_results.csv"
+filename = join(params["path results"],
+                filename)
+results.to_csv(filename)
