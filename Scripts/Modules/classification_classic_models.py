395bc3077bfa6ea37243148f2a7bbf88cfd569c3
Giovanni Lopez <giovannilopez9808@gmail.com>
2022-07-05 00:07:12 -0500

    Add decision and gaussian classification

--- Scripts/Modules/classification_classic_models.py
+++ Scripts/Modules/classification_classic_models.py
@@ -1,5 +1,7 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 from numpy import round
 
@@ -52,6 +54,44 @@ class random_forest_model:
             params: dict) -> None:
         vectors, target = data
         self.model = RandomForestClassifier(**params)
+        self.model.fit(vectors,
+                       target)
+
+    def predict(self,
+                data: list) -> list:
+        vectors, _ = data
+        results = self.model.predict(vectors)
+        return results
+
+
+class Gaussian_naive_model:
+    def __init__(self) -> None:
+        pass
+
+    def run(self,
+            data: list,
+            params: dict) -> None:
+        vectors, target = data
+        self.model = GaussianNB(**params)
+        self.model.fit(vectors,
+                       target)
+
+    def predict(self,
+                data: list) -> list:
+        vectors, _ = data
+        results = self.model.predict(vectors)
+        return results
+
+
+class Decision_tree_model:
+    def __init__(self) -> None:
+        pass
+
+    def run(self,
+            data: list,
+            params: dict) -> None:
+        vectors, target = data
+        self.model = DecisionTreeClassifier(**params)
         self.model.fit(vectors,
                        target)
 
