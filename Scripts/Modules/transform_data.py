fb2142dce81a3e0b8b3c8936c8abd921649a117e
Giovanni Lopez <giovannilopez9808@gmail.com>
2022-07-06 18:57:06 -0500

    Add transform data

---
+++ Scripts/Modules/transform_data.py
@@ -0,0 +1,77 @@
+from Modules.functions import (get_best_similarity_dates,
+                               get_similarity_vectors,
+                               get_cosine_similarity,
+                               comparison_operation,
+                               threshold_filter,
+                               get_hourly_mean,
+                               fill_data,
+                               clean_data)
+from Modules.data_model import (clean_data_model,
+                                SIMA_model)
+from Modules.clear_sky import clear_sky_model
+from pandas import DataFrame
+
+
+class transform_data_model:
+    def __init__(self,
+                 params: dict) -> None:
+        self.params = params
+        self.SIMA_data = SIMA_model(params)
+        self.clean_data = clean_data_model(params)
+
+    def run(self,
+            data: DataFrame) -> DataFrame:
+        self._run_clear_sky()
+        comparison = self._run_comparison(data,
+                                          True)
+        data = self._run_clean_data(data,
+                                    comparison)
+        self._run_cosine_similarity(data)
+        data = self._run_full_data(data)
+        comparison = self._run_comparison(data,
+                                          False)
+        return comparison
+
+    def _run_clear_sky(self):
+        clear_sky = clear_sky_model()
+        clear_sky = clear_sky.run(self.params)
+        self.clear_sky = get_hourly_mean(clear_sky)
+
+    def _run_comparison(self,
+                        data: DataFrame,
+                        use_threshold: bool):
+        operation = self.params["comparison operation"]
+        comparison = comparison_operation(data,
+                                          self.clear_sky,
+                                          operation,
+                                          use_threshold)
+        if use_threshold:
+            comparison = threshold_filter(comparison,
+                                          self.params)
+        return comparison
+
+    def _run_clean_data(self,
+                        data: DataFrame,
+                        comparison: DataFrame):
+        data = clean_data(data,
+                          self.clear_sky,
+                          comparison)
+        return data
+
+    def _run_cosine_similarity(self,
+                               data: DataFrame):
+        cosine = get_cosine_similarity(data,
+                                       self.SIMA_data,
+                                       self.params)
+        similarity_dates = get_best_similarity_dates(cosine,
+                                                     self.params,
+                                                     0)
+        self.similarity_vectors = get_similarity_vectors(self.clean_data,
+                                                         similarity_dates,
+                                                         self.params)
+
+    def _run_full_data(self,
+                       data: DataFrame):
+        full_data = fill_data(data,
+                              self.similarity_vectors)
+        return full_data
---
+++ Scripts/transform_data.py
@@ -0,0 +1,32 @@
+from Modules.transform_data import transform_data_model
+from Modules.functions import get_data_between_hours
+from Modules.params import (get_params,
+                            get_threshold)
+from Modules.data_model import SIMA_model
+from pandas import DataFrame
+
+params = get_params()
+params.update({
+    "comparison operation": "ratio",
+    "clear sky model": "RS",
+    "longitude": -100.22,
+    "latitude": 25.75,
+    "timezone": -5,
+    "top vectors": 30,
+    "station": "Suroeste2",
+    "date": "2021-05-11",
+    "pollutant": "SR",
+    "year": 2021,
+})
+params["threshold"] = get_threshold(params)
+SIMA_data = SIMA_model(params)
+data = SIMA_data.get_data(params["station"],
+                          params["pollutant"],
+                          params["date"])
+data = DataFrame(data)
+data = get_data_between_hours(data,
+                              params)
+
+transform_data = transform_data_model(params)
+comparison = transform_data.run(data)
+print(comparison)
