05de3c242075e09db6f18e07712a4adb82fcb7f8
Giovanni Lopez <giovannilopez9808@gmail.com>
2022-07-07 19:36:09 -0500

    Test perceptron

--- Scripts/neural_models.py
+++ Scripts/neural_models.py
@@ -26,41 +26,45 @@ x_test = []
 gray_scale = 255
 train_files = ls(train_images)
 test_files = ls(test_images)
-# for i,file in enumerate(train_files):
-# filename = join(train_images,
-# file)
-# x = loadtxt(filename)
-# x=x.astype('float32')
-# x /=gray_scale
-# x_train.append(x)
+
+for i, file in enumerate(train_files):
+    ilename = join(train_images,
+                   file)
+    x = loadtxt(filename)
+    x_train.append(x)
+
 for file in test_files:
     filename = join(test_images,
                     file)
     x = loadtxt(filename)
     x_test.append(x)
+
+x_train = array(x_train)
+x_train = x_train.astype('float32')
+x_train /= gray_scale
+
 x_test = array(x_test)
 x_test = x_test.astype('float32')
 x_test /= gray_scale
-print(x_test.shape)
-# model = Sequential([
-# # reshape 28 row * 28 column data to 28*28 rows
-# Flatten(input_shape=(28, 28)),
-# # dense layer 1
-# Dense(256, activation='sigmoid'),
-# # dense layer 2
-# Dense(128, activation='sigmoid'),
-# # output layer
-# Dense(10, activation='sigmoid'),
-# ])
-# model.compile(optimizer='adam',
-# loss='sparse_categorical_crossentropy',
-# metrics=['accuracy'])
-# model.fit(x_train,
-# y_train,
-# epochs=10,
-# batch_size=2000,
-# validation_split=0.2)
-# results = model.evaluate(x_test,
-# y_test,
-# verbose=0)
-# print('test loss, test acc:', results)
+model = Sequential([
+    # reshape 28 row * 28 column data to 28*28 rows
+    Flatten(input_shape=(28, 28)),
+    # dense layer 1
+    Dense(256, activation='sigmoid'),
+    # dense layer 2
+    Dense(128, activation='sigmoid'),
+    # output layer
+    Dense(10, activation='sigmoid'),
+])
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+model.fit(x_train,
+          y_train,
+          epochs=10,
+          batch_size=2000,
+          validation_split=0.2)
+results = model.evaluate(x_test,
+                         y_test,
+                         verbose=0)
+print('test loss, test acc:', results)
