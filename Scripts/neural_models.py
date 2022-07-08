1efd62261caa84b822d8a944c63bcbb2d5963199
Giovanni Lopez <giovannilopez9808@gmail.com>
2022-07-08 05:17:46 -0500

    Perceptron

--- Scripts/neural_models.py
+++ Scripts/neural_models.py
@@ -7,8 +7,8 @@ params.update({
     "comparison operation": argv[1],
     "clear sky model": argv[2],
     "neural model": argv[3],
-    # "hour initial": 8,
-    # "hour final": 20,
+    "hour initial": 9,
+    "hour final": 18,
 })
 
 model = neural_model()
