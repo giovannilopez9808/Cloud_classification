f05e2da554ed29637da125f7475af0456566f286
Giovanni Lopez <giovannilopez9808@gmail.com>
2022-07-07 20:55:06 -0500

    Test perceptron with data

--- Scripts/neural_models.py
+++ Scripts/neural_models.py
@@ -1,54 +1,19 @@
+from Modules.dataset_model import dataset_model
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Activation
 from tensorflow.keras.layers import Flatten
 from tensorflow.keras.layers import Dense
-from numpy import stack, loadtxt, array
-from os.path import join
-from os import listdir
+from Modules.params import get_params
+from sys import argv
 
-
-def ls(path: str) -> list:
-    return sorted(listdir(path))
-
-
-train_path = "datasets/train"
-test_path = "datasets/test"
-train_images = join(train_path,
-                    "images")
-test_images = join(test_path,
-                   "images")
-y_train = loadtxt(join(train_path,
-                       "labels.txt"))
-y_test = loadtxt(join(test_path,
-                      "labels.txt"))
-x_train = []
-x_test = []
-gray_scale = 255
-train_files = ls(train_images)
-test_files = ls(test_images)
-
-for i, file in enumerate(train_files):
-    filename = join(train_images,
-                   file)
-    x = loadtxt(filename)
-    x_train.append(x)
-
-for file in test_files:
-    filename = join(test_images,
-                    file)
-    x = loadtxt(filename)
-    x_test.append(x)
-
-x_train = array(x_train)
-x_train = x_train.astype('float32')
-x_train /= gray_scale
-
-x_test = array(x_test)
-x_test = x_test.astype('float32')
-x_test /= gray_scale
+params = get_params()
+params.update({
+    "comparison operation": argv[1],
+    "clear sky model": argv[2],
+})
+dataset = dataset_model(params)
 model = Sequential([
     # reshape 28 row * 28 column data to 28*28 rows
-    Flatten(input_shape=(28, 28)),
+    Flatten(input_shape=(24)),
     # dense layer 1
     Dense(256, activation='sigmoid'),
     # dense layer 2
@@ -59,12 +24,12 @@ model = Sequential([
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
-model.fit(x_train,
-          y_train,
+model.fit(dataset.train[0],
+          dataset.train[1],
           epochs=10,
           batch_size=2000,
           validation_split=0.2)
-results = model.evaluate(x_test,
-                         y_test,
+results = model.evaluate(dataset.test[0],
+                         dataset.train[1],
                          verbose=0)
 print('test loss, test acc:', results)
