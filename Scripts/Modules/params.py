b29029b7c5a48f285fdade5c15a50d4efb48d57f
Giovanni LOpez <giovannilopez9808@gmail.com>
2022-07-14 05:07:48 -0500

    Better results for LSTM

--- Scripts/Modules/params.py
+++ Scripts/Modules/params.py
@@ -171,7 +171,7 @@ def get_neural_params(params: dict) -> dict:
             },
             "Sureste2": {
                 "validation_split": 0.1,
-                "batch_size": 5,
+                "batch_size": 4,
                 "epochs": 200,
                 "verbose": 1,
             },
