d69c176234a679cd5d3731324dd122830963b0f1
Giovanni Lopez <giovannilopez9808@gmail.com>
2022-07-15 00:45:35 -0500

    Add resume reports ratio RS

---
+++ Scripts/run.sh
@@ -0,0 +1,9 @@
+python get_resume_estimators.py diff GHI SVM
+python get_resume_estimators.py diff GHI KNN
+python get_resume_estimators.py diff GHI "Random forest"
+python get_resume_estimators.py diff GHI "Gaussian naive"
+python get_resume_estimators.py diff GHI "Decision tree"
+python get_resume_estimators.py diff GHI perceptron
+python get_resume_estimators.py diff GHI CNN
+python get_resume_estimators.py diff GHI LSTM
+python get_resume_estimators.py diff GHI RNN
