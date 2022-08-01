50b4eb22d32f4f18a83db93ee559b2569593947d
Giovanni Lopez <giovannilope9808@gmail.com>
2022-07-31 21:55:40 -0500

    Avances

--- Scripts/distribution_per_classification.py
+++ Scripts/distribution_per_classification.py
@@ -21,7 +21,7 @@ datasets = {
 fig, all_axs = plt.subplots(3, 4,
                             sharex=True,
                             sharey=True,
-                            figsize=(12, 8))
+                            figsize=(14, 8))
 all_axs = all_axs.T
 for station, axs in zip(params["stations"],
                         all_axs):
@@ -33,7 +33,7 @@ for station, axs in zip(params["stations"],
         index_vector = dataset.train[1] == index
         vector = dataset.train[0][index_vector]
         vector = vector.flatten()
-        vector = vector[vector > 0]
+        # vector = vector[vector > 0]
         # vector = vector[vector < 1]
         weights = ones_like(vector)*100 / len(vector)
         ax.hist(vector,
@@ -41,8 +41,8 @@ for station, axs in zip(params["stations"],
                 color=color,
                 weights=weights)
         # ax.set_xlim(0, 1)
-        ax.set_xlim(0, 600)
-        ax.set_xticks(linspace(0, 600, 10))
+        ax.set_xlim(0, 1000)
+        ax.set_xticks(linspace(0, 1000, 11))
         ax.set_ylim(0, 30)
         ax.grid(ls="--",
                 color="#000000",
